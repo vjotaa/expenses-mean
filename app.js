35ca02c4c003ed3dade681b930b95c2ea0ccfab4
Angular CLI <angular-cli@angular.io>
2018-04-24 23:20:09 -0400

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const http = require('http');
+const app = express();
+const mongoose = require('mongoose');
+const api = require('./server/routes/expense');
+
+mongoose.connect('mongodb://localhost:27017/expense', (err, res) => {
+  err
+    ? console.log('Error in the database: ', err)
+    : console.log('Connected to: ' + 'mongodb://localhost:27017/expense');
+});
+
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/', api);
+app.get('*'),
+  (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  };
+
+app.listen(port, function() {
+  console.log('Server started....');
+});
